11d67aa5eb56b58077d04529db8543d9af302cf2
SB90-CH <68231803+SB90-CH@users.noreply.github.com>
2024-06-12 18:41:33 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -23,6 +23,7 @@ She is active and wants to experience new things.
 Anna faces some challenges: she is not very organized and lacks the time and desire to handle planning and organization. She has too many different apps on her phone. Many of her ideas for destinations have already been seen, 
 and she is looking for something special. Destinations need to be instagrammable for her. She finds it difficult to unwind.
 Anna is open-minded, carefree, extroverted, cooperative, and a bit vulnerable.
+Information that you must ask for in any case if Anna does not give it to you from her: exact travel dates, duration, budget. After you have found a proper destination for Anna, you should provide her with links to the bookingsites.
 """
 
 my_instance_starter = """
